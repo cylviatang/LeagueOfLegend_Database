8391dacabccba864cb5ff3ec5d1a112d83c0be0b
cylviatang <cylviatt5071@gmail.com>
2018-11-13 11:43:34 -0800

    Initial commit

---
+++ src/League_Database.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class League_Database {
+    private JTextField LeagueOfLegend_DM;
+    private JButton Insert;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
